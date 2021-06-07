6549fd9cfbae16df536e4480a48dea97028cc6b4
Ali Ahmed <83724085+Ali-Ahmed9988@users.noreply.github.com>
2021-06-07 16:55:24 +0200

    Add files via upload

---
+++ spammer.py
@@ -0,0 +1,14 @@
+
+import pyautogui
+import webbrowser as wb
+import time
+wb.open("web.whatsapp.com")
+time.sleep(10)
+for i in range(1000):
+    pyautogui.press("h")
+    pyautogui.press("i")
+   
+   
+
+    
+    pyautogui.press("enter")
